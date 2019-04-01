a05f41deb967879d17510d3021e66d5bb564e523
江俊升 <857012499@qq.com>
2019-04-01 17:28:42 +0800

    test

---
+++ src/main/java/com/zhidian/test/TestTmp.java
@@ -0,0 +1,72 @@
+package com.zhidian.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by 江俊升 on 2019/4/1.
+ */
+public class TestTmp {
+
+    private Integer i = 0;
+
+    private static Integer j = 0;
+
+    private static final AtomicInteger k = new AtomicInteger(0);
+    private static AtomicInteger l = new AtomicInteger(0);
+
+    private void test01(int count) {
+        long l1 = System.currentTimeMillis();
+        Integer i = this.i;
+        for (int c = 0; c < count; c++) {
+            i++;
+        }
+        long l2 = System.currentTimeMillis();
+        System.out.println("局部变量，一共花费：" + (l2 - l1));
+    }
+
+    private void test02(int count) {
+        long l1 = System.currentTimeMillis();
+        for (int c = 0; c < count; c++) {
+            i++;
+        }
+        long l2 = System.currentTimeMillis();
+        System.out.println("成员变量，一共花费：" + (l2 - l1));
+    }
+
+    private void test03(int count) {
+        long l1 = System.currentTimeMillis();
+        for (int c = 0; c < count; c++) {
+            j++;
+        }
+        long l2 = System.currentTimeMillis();
+        System.out.println("静态成员变量，一共花费：" + (l2 - l1));
+    }
+
+    private void test04_1(int count) {
+        long l1 = System.currentTimeMillis();
+        for (int c = 0; c < count; c++) {
+            k.getAndIncrement();
+        }
+        long l2 = System.currentTimeMillis();
+        System.out.println("静态final成员变量，一共花费：" + (l2 - l1));
+    }
+
+    private void test04_2(int count) {
+        long l1 = System.currentTimeMillis();
+        for (int c = 0; c < count; c++) {
+           l.getAndIncrement();
+        }
+        long l2 = System.currentTimeMillis();
+        System.out.println("静态成员变量，一共花费：" + (l2 - l1));
+    }
+
+    public static void main(String[] args) {
+        new TestTmp().test01(1000000000);
+        new TestTmp().test02(1000000000);
+        new TestTmp().test03(1000000000);
+        new TestTmp().test04_1(1000000000);
+        new TestTmp().test04_2(1000000000);
+    }
+
+
+}
