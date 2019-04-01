7716d91f9eb01cc2643154830b365edc74003061
江俊升 <857012499@qq.com>
2019-04-01 17:35:05 +0800

    test

--- src/main/java/com/zhidian/test/TestTmp.java
+++ src/main/java/com/zhidian/test/TestTmp.java
@@ -14,6 +14,8 @@ public class TestTmp {
     private static final AtomicInteger k = new AtomicInteger(0);
     private static AtomicInteger l = new AtomicInteger(0);
 
+    private volatile static AtomicInteger h = new AtomicInteger(0);
+
     private void test01(int count) {
         long l1 = System.currentTimeMillis();
         Integer i = this.i;
@@ -60,12 +62,22 @@ public class TestTmp {
         System.out.println("静态成员变量，一共花费：" + (l2 - l1));
     }
 
+    private void test05(int count) {
+        long l1 = System.currentTimeMillis();
+        for (int c = 0; c < count; c++) {
+            h.getAndIncrement();
+        }
+        long l2 = System.currentTimeMillis();
+        System.out.println("volatile静态成员变量，一共花费：" + (l2 - l1));
+    }
+
     public static void main(String[] args) {
         new TestTmp().test01(1000000000);
         new TestTmp().test02(1000000000);
         new TestTmp().test03(1000000000);
         new TestTmp().test04_1(1000000000);
         new TestTmp().test04_2(1000000000);
+        new TestTmp().test05(1000000000);
     }
 
 
